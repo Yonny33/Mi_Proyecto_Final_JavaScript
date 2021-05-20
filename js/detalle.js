a16d27f7a6d2ef8ab0a645bd0dfa81a89b3c20f5
Yonny33 <yonnisuarez33@gmail.com>
2021-05-20 18:30:32 -0500

    Mi proyecto JavaScript

--- js/detalle.js
+++ js/detalle.js
@@ -2,16 +2,31 @@
 
 $(document).ready(function () {
 
-  //Esta es la instruccion para tomar el id del URL detalle.html?id=<identificador>
+    //Carga los datos que estan en el JSON (info.json) usando AJAX
+  $.ajax({
+    url: "info.json"
+  }).done(function (resultado) {
 
-  //Carga los datos que estan en el JSON (info.json) usando AJAX
+    //Guarda el resultado en una variable
+    eventos = resultado.eventos;
 
-  //Guarda el resultado en una variable
+    //Esta es la instruccion para tomar el id del URL detalle.html?id=<identificador>
+    var id = location.search.match(/id=(\d)*/)[1]
 
-  //Busca el elemento en el arreglo
-
-  //Crea un string que contenga el HTML que describe el detalle del evento
-
-  //Modifica el DOM agregando el html generado dentro del div con id=evento
-
+    //Busca el elemento en el arreglo
+    evento = eventos.find(function (element) {
+      return element.id == id
+    });
+    //Crea un string que contenga el HTML que describe el detalle del evento
+    var html = `
+                <h2>${evento.nombre}</h2>
+                <p>${evento.fecha}</p>
+                <p>Lugar: ${evento.lugar}</p
+                <p>Descripción: ${evento.descripcion}</p>
+                <p>Costo: ${evento.precio}</p>
+                <p>Invitados: ${evento.invitados}</p>
+                `
+//Modifica el DOM agregando el html generado dentro del div con id=evento
+    document.getElementById("evento").innerHTML = html
+  });
 });
