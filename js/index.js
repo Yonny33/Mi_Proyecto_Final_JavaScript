d0fbaff33bfd4dcfea0883d36cdaae7834949593
Yonny33 <yonnisuarez33@gmail.com>
2021-05-22 17:31:27 -0500

    Mi proyecto javascript

--- js/index.js
+++ js/index.js
@@ -1,31 +1,106 @@
-
+var pasados = [];
+var hoy;
+var eventos;
 
 $(document).ready(function () {
 
   //Carga los datos que estan en el JSON (info.json) usando AJAX
+    
+    $.ajax({
+    url: "info.json"
+    }).done(function (resultado) {
 
-  //Guarda el resultado en variables
+    //Guarda el resultado en variables
+    hoy = resultado.fechaActual;
+    eventos = resultado.eventos;
 
-  //Clasifica los eventos segun la fecha actual del JSON
+    //Selecciona los eventos que sean anteriores a la fecha actual del JSON
+        for (var i = 0; i < eventos.length; i++){
+        if (eventos[i].fecha >hoy){
+        pasados.push(eventos[i]);
+        }
+    }
 
-  //Ordena los eventos segun la fecha (los mas cercanos primero)
+    //Ordena los eventos segun la fecha (los mas recientes primero)
+    pasados = pasados.sort(function(x,y){
+        if (x.fecha < y.fecha){
+        return 1;
+        }
+        return -1;
+    });
 
-  //Extrae solo dos eventos
+    //Crea un string que contenga el HTML que describe el detalle del evento
+    var html = ""
 
-  //Ordena los eventos segun la fecha (los mas cercanos primero)
+    //Recorre el arreglo y concatena el HTML para cada evento
+        for (var j = 0; j < pasados.length; j++){
+            html += `
+                <a class = "dos-eventos link" href="detalle.html?id=${pasados[j].id}">
+                <h2>${pasados[j].nombre}</h2>
+                <p>${pasados[j].fecha}</p>
+                <p>Lugar: ${pasados[j].lugar}</p
+                <p>Descripción: ${pasados[j].descripcion}</p>
+                <p>Invitados: ${pasados[j].invitados}</p>
+                `
+    }
 
-  //Extrae solo dos eventos
+    //Modifica el DOM agregando el html generado
+    document.getElementById("proximos").innerHTML = html
 
-  //Crea un string que contenga el HTML que describe el detalle del evento
+    })
+});
 
-  //Recorre el arreglo y concatena el HTML para cada evento
 
-  //Modifica el DOM agregando el html generado
+// Hemos omitido los acentos en los comentarios por compatibilidad
 
-  //Crea un string que contenga el HTML que describe el detalle del evento
+var pasados = [];
+var hoy;
+var eventos;
 
-  //Recorre el arreglo y concatena el HTML para cada evento
+$(document).ready(function () {
 
-  //Modifica el DOM agregando el html generado
+  //Carga los datos que estan en el JSON (info.json) usando AJAX
+    
+    $.ajax({
+    url: "info.json"
+    }).done(function (resultado) {
 
+    //Guarda el resultado en variables
+    hoy = resultado.fechaActual;
+    eventos = resultado.eventos;
+
+    //Selecciona los eventos que sean anteriores a la fecha actual del JSON
+        for (var i = 0; i < eventos.length; i++){
+        if (eventos[i].fecha < hoy){
+        pasados.push(eventos[i]);
+        }
+    }
+
+    //Ordena los eventos segun la fecha (los mas recientes primero)
+    pasados = pasados.sort(function(x,y){
+        if (x.fecha < y.fecha){
+        return 1;
+        }
+        return -1;
+    });
+
+    //Crea un string que contenga el HTML que describe el detalle del evento
+    var html = ""
+
+    //Recorre el arreglo y concatena el HTML para cada evento
+        for (var j = 0; j < pasados.length; j++){
+            html += `
+                <a class = "dos-eventos link" href="detalle.html?id=${pasados[j].id}">
+                <h2>${pasados[j].nombre}</h2>
+                <p>${pasados[j].fecha}</p>
+                <p>Lugar: ${pasados[j].lugar}</p
+                <p>Descripción: ${pasados[j].descripcion}</p>
+                <p>Invitados: ${pasados[j].invitados}</p>
+                `
+    }
+
+    //Modifica el DOM agregando el html generado
+    document.getElementById("pasados").innerHTML = html
+
+    })
 });
