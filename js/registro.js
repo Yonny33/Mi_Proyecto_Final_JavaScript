de22605ac6708f93869c5483476600010541e4e9
Yonny33 <yonnisuarez33@gmail.com>
2021-05-21 21:33:17 -0500

    Mi proyecto JavaScript

--- js/registro.js
+++ js/registro.js
@@ -2,6 +2,37 @@
 
 function validar(formulario) {
 
-  //Expresion regular del correo
+  if (formulario.nombre.value.trim().length == 0) {
+    alert("Nombre obligatorio");
+    return false;
+  }
+
+  if (formulario.contraseña.value.trim().length == 0) {
+    alert("Contraseña obligatorio");
+    return false;
+  }
+    
+  if (formulario.contraseña.value != formulario.confirmacion.value) {
+    alert("Confirmacion obligatorio");
+    return false;
+  }
+
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!re.test(formulario.email.value)) {
+    alert("Email invalido");
+    return false;
+  }
+
+  if (formulario.pais.value == "") {
+    alert("Pais es obligatorio");
+    return false;
+  }
+
+  if (!formulario.terminos.checked) {
+    alert("Debe aceptar los terminos y condiciones");
+    return false;
+  }
+
+  return true;
 
 }
